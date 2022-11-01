c0fcef3445427cb5fcfae78efdaa60e1a3879a77
Darkmind2000 <111960024+Darkmind2000@users.noreply.github.com>
2022-11-01 01:09:54 -0700

    Add files via upload

--- crawl.py
+++ crawl.py
@@ -1,3 +1,4 @@
+
 import requests
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -32,8 +33,10 @@ count=1
 r=requests.get('https://github.com/search?p={}&q={}&type=Repositories'.format(count,search_key))
     # Parsing the HTML
 soup = BeautifulSoup(r.content, 'html.parser')
-res=int(soup.find('a',{"aria-label":"Page 100"}).text)
-for x in range(res+1):
+res=soup.select_one('em[class="current"]')
+res1=int(res.get('data-total-pages'))
+
+for x in range(res1+1):
   #set search key in Url and rady to use in BeautifulSoup
   r=requests.get('https://github.com/search?p={}&q={}&type=Repositories'.format(count,search_key))
   soup=BeautifulSoup(r.content,'html.parser')
@@ -43,9 +46,9 @@ for x in range(res+1):
   first_link = soup.find_all("a",{"class":"v-align-middle"})
   for i in first_link:
     p='https://github.com'+ str(i.get('href'))
-    sql = "INSERT INTO DataScraping (Link) VALUES (%s)"
+    # sql = "INSERT INTO DataScraping (Link) VALUES (%s)"
 
-    mycursor.execute(sql,p)
+    # mycursor.execute(sql,(p,))
 
   #Find Programmming Language & insert to Table mysql
   ProgramminfLanguage = soup.find_all('li', {"class":"repo-list-item hx_hit-repo d-flex flex-justify-start py-4 public source"})
@@ -54,18 +57,22 @@ for x in range(res+1):
     k=i.select_one('span[itemprop="programmingLanguage"]')
     if (k == None ) :
         j='None'
-        mycursor.execute("insert into DataScraping(ProgrammingLanguage) values(%s)", j)
+        # mycursor.execute("insert into DataScraping(ProgrammingLanguage) values(%s)", (j,))
     else:
-        
-        mycursor.execute("insert into DataScraping(ProgrammingLanguage) values(%s)", k.getText().lower())
+        j=k.getText().lower()
+        # mycursor.execute("insert into DataScraping(ProgrammingLanguage) values(%s)", (k.getText().lower(),))
   #Find Stargazer & Insert to Table mysql 
   star = soup.find_all("a" , {"class":"Link--muted"})
   for l in star:
     if ("issue" not in l.get_text().strip() ):
-        print()
-        mycursor.execute("insert into DataScraping(Stargazer) values(%s)", l.get_text().strip())
+        l1=l.get_text().strip()
+        # mycursor.execute("insert into DataScraping(Stargazer) values(%s)", (l.get_text().strip(),))
+  val=[str(p),str(j),str(l)]
+  sql="INSERT INTO DataScraping (Link,ProgrammingLanguage,Stargazer) VALUES (%s,%s,%s);"
+  mycursor.executemany(sql,((val[0], val[1], val[2]),))
+  mydb.commit()      
   count=count+1
-  Bouton=driver.find_element(By.XPATH,'/html/body/div[4]/main/div/div[3]/div/div[2]/div/a[4]')
+  Bouton=driver.find_element(By.XPATH,'/html/body/div[4]/main/div/div[3]/div/div[3]/div/a[7]')
   Bouton.click()
 time.sleep(10)
 driver.quit()
